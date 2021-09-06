299049d2e3d753d6b4d6c7f4ffb4804116a2b953
Asbudi <asbudianugrahpatimasang@gmail.com>
2021-09-06 12:50:16 +0800

    mengedit file koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,13 +1,13 @@
 <?php  
-// $servername = "localhost";
-// $username = "u1438856_beedecal";
-// $password = "riswanboss9999";
-// $dbname = "u1438856_beedecal";
-
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "beedecal";
+$username = "u1438856_beedecal";
+$password = "riswanboss9999";
+$dbname = "u1438856_beedecal";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "beedecal";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
